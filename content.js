b32c3fcc8185ffcab0445cb8c7529fec47cce122
andrew@thiesen.co <andrew@thiesen.co>
2023-06-30 09:40:46 -0400

    1. update content.json, for the tunnel vision effect to occur based on two inputs: 1. the user selects a paragraph, or 2, the entire page where there are <p> </p> tags
    2. update style.css to reflect additional changes.

--- content.js
+++ content.js
@@ -1,14 +1,76 @@
-// Listen for scroll events
-window.addEventListener("scroll", function () {
-  // Calculate the position of the paragraph relative to the viewport
-  var paragraph = document.getElementById("your-paragraph-id");
-  var rect = paragraph.getBoundingClientRect();
-  var top = rect.top;
-  var bottom = rect.bottom;
+// Listen for scroll and selection events
+window.addEventListener("scroll", handleScroll);
+document.addEventListener("selectionchange", handleSelection);
 
-  // Update the CSS properties to achieve the "tunnel vision" effect
-  // Adjust the code below based on your desired visual effect
-  var tunnelElement = document.getElementById("your-tunnel-element-id");
-  tunnelElement.style.top = top + "px";
-  tunnelElement.style.height = bottom - top + "px";
-});
+// Function to handle scroll events
+function handleScroll() {
+  // Check if a paragraph is selected
+  if (selectedParagraph) {
+    applyTunnelVision(selectedParagraph);
+  }
+}
+
+// Function to handle selection events
+function handleSelection() {
+  var selection = window.getSelection();
+  var selectedText = selection.toString().trim();
+
+  // Check if any text is selected
+  if (selectedText !== "") {
+    // Check if the selection is within a paragraph
+    var paragraph = selection.anchorNode.parentNode;
+    if (paragraph.nodeName === "P") {
+      selectedParagraph = paragraph;
+      applyTunnelVision(selectedParagraph);
+    }
+  } else {
+    // If no text is selected, reset the selectedParagraph variable
+    selectedParagraph = null;
+    removeTunnelVision();
+  }
+}
+
+// Function to apply the tunnel vision effect to a selected paragraph
+function applyTunnelVision(paragraph) {
+  // Clear any existing tunnel vision elements
+  removeTunnelVision();
+
+  // Create a new tunnel vision element
+  var tunnelElement = document.createElement("div");
+  tunnelElement.id = "tunnel-vision-effect";
+  tunnelElement.style.position = "fixed";
+  tunnelElement.style.left = "0";
+  tunnelElement.style.right = "0";
+  tunnelElement.style.top = paragraph.offsetTop + "px";
+  tunnelElement.style.height = paragraph.offsetHeight + "px";
+  tunnelElement.style.background = "rgba(0, 0, 0, 0.5)";
+  tunnelElement.style.zIndex = "9999";
+
+  // Apply the tunnel vision effect
+  paragraph.style.position = "relative";
+  paragraph.style.zIndex = "99999";
+  paragraph.parentNode.insertBefore(tunnelElement, paragraph);
+}
+
+// Function to remove the tunnel vision effect
+function removeTunnelVision() {
+  var tunnelElement = document.getElementById("tunnel-vision-effect");
+  if (tunnelElement) {
+    var paragraph = tunnelElement.nextSibling;
+    tunnelElement.parentNode.removeChild(tunnelElement);
+    paragraph.style.position = "";
+    paragraph.style.zIndex = "";
+  }
+}
+
+// Check if the entire page is selected
+var pageSelection = window.getSelection().toString().trim();
+if (pageSelection === "") {
+  var paragraphs = document.getElementsByTagName("p");
+  for (var i = 0; i < paragraphs.length; i++) {
+    paragraphs[i].addEventListener("click", function (event) {
+      selectedParagraph = event.target;
+      applyTunnelVision(selectedParagraph);
+    });
+  }
+}
